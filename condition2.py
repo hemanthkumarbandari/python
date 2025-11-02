e5910ea418dcd3d913e68522670ca1cb12c67271
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-02 18:28:15 +0530

    addedfile

--- condition2.py
+++ condition2.py
@@ -6,7 +6,7 @@ if num % 2 == 0:
 else:
     print("The number is Odd")
     """
-# Check if a number is positive, negative, or zero
+"""# Check if a number is positive, negative, or zero
 n = int(input("Enter a number: "))
 
 if n > 0:
@@ -14,4 +14,19 @@ if n > 0:
 elif n < 0:
     print("Negative number")
 else:
-    print("Number is Zero")
+    print("Number is Zero") """
+
+# Check voting eligibility
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+if a >= b and a >= c:
+    print(a, "is the largest")
+elif b >= a and b >= c:
+    print(b, "is the largest")
+else:
+    print(c, "is the largest")
