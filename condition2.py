95ce84595b8f0c09cb119bcf0264055bb9ee7ca9
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-02 18:25:54 +0530

    addedfile

--- condition2.py
+++ condition2.py
@@ -1,7 +1,17 @@
-# Check if a number is even or odd
+"""# Check if a number is even or odd
 num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print("The number is Even")
 else:
     print("The number is Odd")
+    """
+# Check if a number is positive, negative, or zero
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Positive number")
+elif n < 0:
+    print("Negative number")
+else:
+    print("Number is Zero")
