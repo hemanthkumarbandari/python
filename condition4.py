b4fe1e5d94f52ee0c9a1f0250d9570d4b904773d
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-04 18:28:47 +0530

    addedfile

--- condition4.py
+++ condition4.py
@@ -1,4 +1,4 @@
-a = int(input("Enter first number: "))
+"""a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
@@ -7,4 +7,20 @@ if (a >= b and a <= c) or (a <= b and a >= c):
 elif (b >= a and b <= c) or (b <= a and b >= c):
     print("Second largest is:", b)
 else:
-    print("Second largest is:", c)
+    print("Second largest is:", c)"""
+
+cost = float(input("Enter Cost Price: "))
+sell = float(input("Enter Selling Price: "))
+
+if sell > cost:
+    profit = sell - cost
+    percent = (profit / cost) * 100
+    print("Profit =", profit)
+    print("Profit Percentage =", percent, "%")
+elif sell < cost:
+    loss = cost - sell
+    percent = (loss / cost) * 100
+    print("Loss =", loss)
+    print("Loss Percentage =", percent, "%")
+else:
+    print("No Profit No Loss")
