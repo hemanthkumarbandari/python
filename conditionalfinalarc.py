6da73d14e1baa3f2bfd748021031d920ec5b9ef2
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-05 20:03:41 +0530

    addedfile

--- conditionalfinalarc.py
+++ conditionalfinalarc.py
@@ -1,4 +1,4 @@
-# Input marks for 5 subjects
+"""# Input marks for 5 subjects
 m1 = int(input("Enter marks of Subject 1: "))
 m2 = int(input("Enter marks of Subject 2: "))
 m3 = int(input("Enter marks of Subject 3: "))
@@ -18,4 +18,27 @@ elif avg >= 60:
 elif avg >= 40:
     print("Grade: C (Pass)")
 else:
-    print("Grade: F (Fail)")
+    print("Grade: F (Fail)")"""
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /, %): ")
+
+if op == '+':
+    print("Result =", a + b)
+elif op == '-':
+    print("Result =", a - b)
+elif op == '*':
+    print("Result =", a * b)
+elif op == '/':
+    if b != 0:
+        print("Result =", a / b)
+    else:
+        print("Error: Division by zero not allowed")
+elif op == '%':
+    if b != 0:
+        print("Result =", a % b)
+    else:
+        print("Error: Division by zero not allowed")
+else:
+    print("Invalid Operator")
