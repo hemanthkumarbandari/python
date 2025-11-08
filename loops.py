624fc76fafa850c51847a73a31e8ccd110b4b1d6
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-08 20:42:38 +0530

    addedfile

--- loops.py
+++ loops.py
@@ -5,7 +5,11 @@ for i in range(1, 21):
     if i % 2 == 0:
         print(i)"""
 
-sum = 0
+"""sum = 0
 for i in range(1, 11):
     sum += i
-print("Sum =", sum)
+print("Sum =", sum)"""
+
+word = input("Enter word: ")
+for ch in word:
+    print(ch)
