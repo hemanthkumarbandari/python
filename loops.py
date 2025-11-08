8a2af6ba46db13ef67be7032456791b3bf551f3d
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-08 20:39:01 +0530

    addedfile

--- loops.py
+++ loops.py
@@ -1,7 +1,11 @@
-for i in range(1, 11):
+"""for i in range(1, 11):
     print(i)
 
 for i in range(1, 21):
     if i % 2 == 0:
-        print(i)
+        print(i)"""
 
+sum = 0
+for i in range(1, 11):
+    sum += i
+print("Sum =", sum)
