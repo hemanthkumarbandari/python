b69abe3bc9a484c88e377f51bca72b9f563fd939
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-12 05:04:56 +0530

    addedfile

--- name.py
+++ name.py
@@ -1,8 +1,18 @@
 """name = input("Enter your name: ")
 print("Hello, " + name + "! Welcome to Python.")"""
 
-num = int(input("Enter a number: "))
+"""num = int(input("Enter a number: "))
 if num % 2 == 0:
     print("Even number")
 else:
-    print("Odd number")
+    print("Odd number")"""
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum2 + sum2
+
+print(sum1)
+print(sum2)
+print(sum3)
+
+
