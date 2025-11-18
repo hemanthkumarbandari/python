fc717f209441d4b505354fc56e4adc164fa4105c
hemanth <hemanthkumarbandari001@gmail.com>
2025-11-18 21:45:40 +0530

    addedfille

--- Largest Number.py
+++ Largest Number.py
@@ -1,4 +1,4 @@
-a = int(input("Enter first number: "))
+"""a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
@@ -7,4 +7,10 @@ if a >= b and a >= c:
 elif b >= a and b >= c:
     print(b, "is the largest")
 else:
-    print(c, "is the largest")
+    print(c, "is the largest")"""
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
