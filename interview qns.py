d80c25d5fd52554ebcd3fc73a3af4753d518ef65
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-16 18:10:51 +0530

    addedfile

--- interview qns.py
+++ interview qns.py
@@ -1,3 +1,4 @@
+"""
 #valid triangle 
 a = int(input("Enter side a:"))
 b = int(input("Enter side b:"))
@@ -6,3 +7,8 @@ if a+b>c and b+c > a and a+c>b:
     print ("Triangle is valid")
 else:
     print("invalid")
+"""
+# divisible by 3 and 5
+num = int(input("Enter a Number;"))
+if num % 3 == 0 and num % 5 == 0:
+    print("divisible1")
