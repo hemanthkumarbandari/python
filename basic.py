daca645decccc47383f0af3da01b5965885da382
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-19 22:58:30 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -11,3 +11,14 @@ elif num % 3 == 0:
     print("The number is a multiple of 3")
 else:
     print("The number is not a multiple of 2 or 3")
+balance = 10000
+amount = int(input("Enter withdrawal amount: "))
+
+if amount > balance:
+    print("Insufficient balance")
+elif amount % 100 != 0:
+    print("Enter amount in multiples of 100")
+else:
+    balance -= amount
+    print("Withdraw successful")
+    print("Remaining Balance:", balance)
