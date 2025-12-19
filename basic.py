5966789d0dfbf0f778ca30fa246c43aa02f0eff7
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-19 23:00:13 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -43,3 +43,9 @@ elif temp >= 20:
     print("Warm")
 else:
     print("Cold")
+year = int(input("Enter year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
