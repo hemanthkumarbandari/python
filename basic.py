092b5bb8c482dc38768f5678640e62dccf0b57c6
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-19 22:57:38 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,3 +1,13 @@
 print ("hey world")
 print("Python")
 print ("Hi")
+num = int(input("Enter a number: "))
+
+if num % 2 == 0 and num % 3 == 0:
+    print("The number is a multiple of both 2 and 3")
+elif num % 2 == 0:
+    print("The number is a multiple of 2")
+elif num % 3 == 0:
+    print("The number is a multiple of 3")
+else:
+    print("The number is not a multiple of 2 or 3")
