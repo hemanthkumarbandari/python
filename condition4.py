6630be3cee849700259f7e509127be559ab7f51e
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-20 23:08:37 +0530

    Update condition4.py

--- condition4.py
+++ condition4.py
@@ -48,3 +48,13 @@ elif num < 100:
     print("Double-digit")
 else:
     print("More than two digits")
+ch = input("Enter a character: ")
+
+if 'A' <= ch <= 'Z':
+    print("Uppercase letter")
+elif 'a' <= ch <= 'z':
+    print("Lowercase letter")
+elif '0' <= ch <= '9':
+    print("Digit")
+else:
+    print("Special character")
