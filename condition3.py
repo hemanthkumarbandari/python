ffbb45a9c960f246b89653ec6fe0aec2451d4579
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-22 14:12:27 +0530

    Update condition3.py

--- condition3.py
+++ condition3.py
@@ -80,4 +80,10 @@ elif num == 7:
 else:
     print("Invalid number")
 """
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
 
