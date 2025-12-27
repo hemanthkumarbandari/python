4ea63627ec243e593e2867a263a34aaa08ba80d2
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-27 08:01:06 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -10,9 +10,11 @@ for i in range(1, 11):
     sum += i
 print("Sum =", sum)"""
 
+"""
 word = input("Enter word: ")
 for ch in word:
     print(ch)
 str1 = ("red")
 str2 = ("orange")
 print str1+str2
+"""
