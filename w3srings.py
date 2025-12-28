ed4e5949201b6be49857854ece0f25a1ded46a1b
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-28 22:04:07 +0530

    addedfile

--- w3srings.py
+++ w3srings.py
@@ -1,3 +1,4 @@
+"""
 a = ("Hemanth Kumar")
 print (a[3:9])
 
@@ -7,3 +8,13 @@ print (a.lower())
 #strip to remove spaces
 print (a.replace("H","Y"))
 print (a.split(","))
+"""
+"""
+a = "Hello"
+b = "World"
+c = a + " " + b
+print (c)
+"""
+print (10>7)
+print (10==7)
+print (10<7)
