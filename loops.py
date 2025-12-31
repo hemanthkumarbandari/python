63d792c1c5b75130442adbf5813c18f6745c6f31
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2025-12-31 20:37:04 +0530

    addedfile

--- loops.py
+++ loops.py
@@ -35,3 +35,13 @@ while n > 0:
     n //= 10
 
 print("Digits =", count)
+
+n = 1234
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n //= 10
+
+print("Reversed =", rev)
