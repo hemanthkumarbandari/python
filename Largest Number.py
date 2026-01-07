ace8bc23cee9d9b7853b0db91e02fd34aba42515
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-01-07 23:31:28 +0530

    Update Largest Number.py

--- Largest Number.py
+++ Largest Number.py
@@ -8,11 +8,11 @@ elif b >= a and b >= c:
     print(b, "is the largest")
 else:
     print(c, "is the largest")"""
-
+"""
 num = int(input("Enter a number: "))
 if num % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
-
+"""
 """print ("hello")"""
