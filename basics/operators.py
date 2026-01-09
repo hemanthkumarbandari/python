6b8d71d0a8f575d9a1cc59974debe83cefa7dc21
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-01-09 23:47:04 +0530

    addedfile

---
+++ basics/operators.py
@@ -0,0 +1,15 @@
+print(3)
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+print(x is not y)
