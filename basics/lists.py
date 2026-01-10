58ba2b01274207c2d080859b06c00fa09775a60a
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-01-10 23:50:01 +0530

    addedfile

--- basics/lists.py
+++ basics/lists.py
@@ -4,3 +4,9 @@ print(thislist[-4:-1])
 thislist = ["apple", "banana", "cherry"]
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list")
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+thislist[1:3] = ["blackcurrant", "watermelon"]
+
+print(thislist)
