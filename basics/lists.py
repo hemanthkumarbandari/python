65a8e3e35070c49394ddb8fbe8c904f63920c030
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-01-22 23:58:20 +0530

    addedfile

--- basics/lists.py
+++ basics/lists.py
@@ -78,3 +78,10 @@ thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.sort(key = str.lower)
 
 print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+
+thislist.reverse()
+
+print(thislist) 
+
