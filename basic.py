be492116635d04e11f76e25e4901d2d61ce16d1d
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-01-25 22:43:38 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -54,11 +54,10 @@ else:
 a = int(input("Enter A: "))
 b = int(input("Enter B: "))
 c = int(input("Enter C: "))
-"""
+
 if a >= b and a >= c:
     print(a, "is largest")
 elif b >= a and b >= c:
     print(b, "is largest")
 else:
     print(c, "is largest")
-"""
