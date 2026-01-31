c588a6a31380adac2f40cdf07ee3640c38903aea
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-02-01 00:05:33 +0530

    addedfile

--- datatypes/Datatypes/sets.py
+++ datatypes/Datatypes/sets.py
@@ -12,3 +12,12 @@ print (myset)
 
 s = {"car", "bike", "auto", False, 0, 1}
 print (s)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1) # values changes everytime
+print(set2)
+print(set3) 
+
