4bcccc082544ed6b223d801efe9ccab2e77dbc0d
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-02-19 23:59:47 +0530

    addedfile

--- datatypes/Datatypes/dictionarys.py
+++ datatypes/Datatypes/dictionarys.py
@@ -23,3 +23,16 @@ thisdict =	{
 x = thisdict.get("model")
 print(x)
 
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
