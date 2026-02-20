2dc34a6df16745243bb074d0bdec98929696de86
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-02-20 23:32:26 +0530

    addedfile

--- datatypes/Datatypes/dictionarys.py
+++ datatypes/Datatypes/dictionarys.py
@@ -86,3 +86,14 @@ thisdict = {
 }
 if "model" in thisdict:
   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+print(thisdict)
+
+
