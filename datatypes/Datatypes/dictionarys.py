af0663aff1789beda48d889296f1f65565756bfe
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-03-01 21:33:40 +0530

    addedfile

--- datatypes/Datatypes/dictionarys.py
+++ datatypes/Datatypes/dictionarys.py
@@ -236,3 +236,11 @@ thisdict =	{
 }
 for x in thisdict:
   print(thisdict[x])
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
