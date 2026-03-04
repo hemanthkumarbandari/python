9aeb087084c85790b0a500c929aa8e60e84751ee
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-03-04 16:53:17 +0530

    addedfile

--- datatypes/Datatypes/dictionarys.py
+++ datatypes/Datatypes/dictionarys.py
@@ -252,3 +252,20 @@ thisdict = {
 }
 mydict = thisdict.copy()
 print(mydict)
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily)
