0dd2ca126ed478aadf3f1ff67750f5aa18b01a74
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-03-14 20:09:39 +0530

    addedfile

---
+++ functions.py
@@ -0,0 +1,20 @@
+#Armstrong
+def armstrong(n):
+    s = 0
+    for i in str(n):
+        s += int(i) ** len(str(n))
+    if s == n:
+        print("Armstrong")
+    else:
+        print("Not Armstrong")
+
+armstrong(153)
+
+#Palindrome Strings
+def find_palindrome(lst):
+    for i in lst:
+        if i == i[::-1]:
+            print(i)
+
+words = ["madam","apple","level","python","radar"]
+find_palindrome(words)
