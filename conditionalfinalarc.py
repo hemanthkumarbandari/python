45094e878a72c2eae7f532578d9d136eed9445d3
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-03-16 00:44:19 +0530

    addedfile

--- conditionalfinalarc.py
+++ conditionalfinalarc.py
@@ -81,3 +81,12 @@ has_discount_code = True
 
 if (age < 18 or age > 65) and not is_student or has_discount_code:
   print("Discount applies!")
+
+age = 25
+is_student = False
+has_discount_code = False
+
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+  print("Discount applies!")
+else:
+  print("Discount does not applies")
