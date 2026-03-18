5a1ae37eff1727e7e5fd60570875a864ecc58393
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-03-18 23:11:59 +0530

    addedfile

--- conditionalfinalarc.py
+++ conditionalfinalarc.py
@@ -97,4 +97,21 @@ b = 200
 if b > a:
   pass
 
-# having an empty if statement like this, would raise an error without the pass statement
+#match
+
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
