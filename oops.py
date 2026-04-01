e46da1103c233f229fc0b86cfd7559cac19daf39
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-04-01 20:10:33 +0530

    addedfile

--- oops.py
+++ oops.py
@@ -34,3 +34,15 @@ class Dog(Animal):
 
 d = Dog()
 d.sound()
+
+class Vehicle:
+    def start(self):
+        print("Vehicle started")
+
+class Car(Vehicle):
+    def drive(self):
+        print("Car is driving")
+
+c = Car()
+c.start()
+c.drive()
