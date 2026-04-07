8227b5d4cf374a1394cbf309357607ab3cc2a667
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-04-07 20:48:44 +0530

    addedfile

--- oops/abstraction.py
+++ oops/abstraction.py
@@ -13,3 +13,31 @@ class C1(Car):
 
 ob1 = C1()
 ob1.stop()
+
+from abc import ABC, abstractmethod
+
+
+# Abstract Class
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass   # no implementation here
+
+
+# Child Class
+class Car(Vehicle):
+
+    def start(self):
+        print("Car starts with key")
+
+    def stop(self):
+        print("Car stopped")
+
+
+# Object Creation
+car = Car()
+
+# Method Calls
+car.start()
+car.stop()
