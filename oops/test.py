89dbef81816866df27cea2a33231958ce1dc7171
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-04-21 22:56:46 +0530

    addedfile

--- oops/test.py
+++ oops/test.py
@@ -5,3 +5,17 @@ def set_age(self, age):
 def deposit(self, amount):
     if amount > 0:
         self.__balance += amount
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print(self.name, self.marks)
+
+s1 = Student("Ravi", 80)
+s2 = Student("Ram", 60)
+
+s1.display()
+s2.display()
