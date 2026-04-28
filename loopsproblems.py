becd5646246e876aa8d67ba5a13be30d41b80237
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-04-28 13:51:20 +0530

    addedfile

--- loopsproblems.py
+++ loopsproblems.py
@@ -10,3 +10,16 @@ while n > 0:
     n //= 10
 
 print(rev)
+
+# Palindrome Number
+# "Check whether a number is a palindrome using a loop."
+
+n = 121
+temp = n
+rev = 0
+
+while n > 0:
+    rev = rev * 10 + (n % 10)
+    n //= 10
+
+print("Palindrome" if temp == rev else "Not Palindrome")
