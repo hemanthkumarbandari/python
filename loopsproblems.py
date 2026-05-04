a43ff0721c190d1b5f9503e61bc44e60a6ed3253
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-05-05 04:36:18 +0530

    addedfile

--- loopsproblems.py
+++ loopsproblems.py
@@ -105,3 +105,17 @@ for i in range(1, n + 1):
     for j in range(i):
         print("*", end=" ")
     print()
+
+# Largest Digit in a Number
+# "Find the largest digit present in a given number using loops."
+
+n = 73952
+max_digit = 0
+
+while n > 0:
+    digit = n % 10
+    if digit > max_digit:
+        max_digit = digit
+    n //= 10
+
+print(max_digit)
