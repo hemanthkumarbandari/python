45acf2cceb45f45dbe14575935e5574b535f0007
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-05-14 00:34:22 +0530

    addedfile

--- loopsproblems.py
+++ loopsproblems.py
@@ -239,3 +239,20 @@ while num > 0:
     num = num // 10
 
 print("Total Digits =", count)
+
+# Palindrome Number Check
+
+num = int(input("Enter number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
