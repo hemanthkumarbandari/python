360c56a4f724676ac73308670bc389449ee80e9f
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-05-15 17:32:56 +0530

    addedfile

--- loopsproblems.py
+++ loopsproblems.py
@@ -256,3 +256,13 @@ if original == reverse:
     print("Palindrome")
 else:
     print("Not Palindrome")
+
+# Star Pyramid Pattern
+
+n = int(input("Enter rows: "))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    
+    print()
