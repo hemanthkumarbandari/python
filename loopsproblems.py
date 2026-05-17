8992890156fa92b6eeb61276e215b418bb6a257b
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-05-17 20:45:55 +0530

    addedfile

--- loopsproblems.py
+++ loopsproblems.py
@@ -280,3 +280,22 @@ for i in range(1, n + 1):
         print("*", end=" ")
     
     print()
+
+# Prime Number Check
+
+num = int(input("Enter number: "))
+
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
