6940bf07ae533dba55a44172755b7419cebc34c9
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-20 23:32:16 +0530

    addedfile

--- mainproblems.py
+++ mainproblems.py
@@ -1,5 +1,14 @@
+#Reverse a String
 s = input("Enter a string: ")
 
 reverse = s[::-1]
 
 print("Reversed String:", reverse)
+
+#Check Palindrome
+s = input("Enter a string: ")
+
+if s == s[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
