37a3541a5aeca00144fc14c74f0efd1fb100d4dd
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-20 23:34:05 +0530

    addedfile

--- mainproblems.py
+++ mainproblems.py
@@ -12,3 +12,14 @@ if s == s[::-1]:
     print("Palindrome")
 else:
     print("Not Palindrome")
+
+# Count Vowels
+s = input("Enter a string: ")
+
+count = 0
+
+for ch in s.lower():
+    if ch in "aeiou":
+        count += 1
+
+print("Number of vowels:", count)
