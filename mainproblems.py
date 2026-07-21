1cf1277f1590622ab24532ea2cf3fc578c20a4ca
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-21 23:29:00 +0530

    addedfile

--- mainproblems.py
+++ mainproblems.py
@@ -46,3 +46,11 @@ for ch in s:
         freq[ch] = 1
 
 print(freq)
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+if sorted(s1) == sorted(s2):
+    print("Anagram")
+else:
+    print("Not Anagram")
