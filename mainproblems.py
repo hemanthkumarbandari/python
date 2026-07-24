e22dde6f2edcfd42b03db8a9b543da6f4c5c9329
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-24 23:18:34 +0530

    Update mainproblems.py

--- mainproblems.py
+++ mainproblems.py
@@ -71,3 +71,13 @@ def palindrome(text):
         return False
 
 print(palindrome("madam"))
+
+s = input("Enter a string: ")
+
+result = ""
+
+for ch in s:
+    if ch not in result:
+        result += ch
+
+print("After removing duplicates:", result)
