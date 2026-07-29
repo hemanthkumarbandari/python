1305ced84a49562752f603de22f6ad96b1123793
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-29 20:11:02 +0530

    Update mainproblems.py

--- mainproblems.py
+++ mainproblems.py
@@ -110,3 +110,19 @@ else:
 s = input("Enter a string: ")
 
 print("Uppercase:", s.upper())
+
+s = input("Enter a string: ")
+
+letters = digits = special = 0
+
+for ch in s:
+    if ch.isalpha():
+        letters += 1
+    elif ch.isdigit():
+        digits += 1
+    else:
+        special += 1
+
+print("Letters:", letters)
+print("Digits:", digits)
+print("Special Characters:", special)
