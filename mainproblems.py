2590262320b6b699258e91c75a9b05d3b69f43c8
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-07-31 21:29:36 +0530

    Update mainproblems.py

--- mainproblems.py
+++ mainproblems.py
@@ -136,3 +136,15 @@ for ch in s:
         result += ch
 
 print("After removing spaces:", result)
+
+s = input("Enter a sentence: ")
+
+words = s.split()
+
+largest = words[0]
+
+for word in words:
+    if len(word) > len(largest):
+        largest = word
+
+print("Largest Word:", largest)
