7c2e6ebf6aa4c4d4f30a2b442f5e5f3251002155
Hemanth Kumar Bandari <hemanthkumarbandari001@gmail.com>
2026-09-25 23:51:29 +0530

    addefile

--- patterns.py
+++ patterns.py
@@ -32,3 +32,10 @@ for i in range(n):
     for j in range(1,n-i+1):
         print(j, end="")
     print()
+
+n = 5
+
+for i in range(n):
+    for j in range(n-i):
+        print(j+1, end="")
+    print()
